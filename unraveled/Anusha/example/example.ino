f941b9cff40be1e33854ef135e440c658b3a93e9
Anusha Datar <anushadatar@gmail.com>
2019-04-21 10:34:03 -0400

    still fighting with the linker

--- unraveled/Anusha/example/example.ino
+++ unraveled/Anusha/example/example.ino
@@ -5,33 +5,31 @@
 static int thread1_flag, thread2_flag;
 static struct thread thread1, thread2;
 
-
-static int thread1(thread thread)
+static int thread1_function(struct thread* t)
 {
-  THREAD_BEGIN(thread);
+  THREAD_BEGIN(t);
 
   while(1) {
   
-    PT_WAIT_UNTIL(thread, thread2_flag != 0);
+    THREAD_WAIT_UNTIL(t, thread2_flag != 0);
     printf("Thread 1 is running\n");
 
     thread2_flag = 0;
     thread1_flag = 1;
   }
-  THREAD_END(thread);
+  THREAD_END(t);
 }
 
-static int thread2(struct thread *thread)
+static int thread2_function(struct thread* t )
 {
-  THREAD_BEGIN(thread);
+  THREAD_BEGIN(t);
   while(1) {
     thread2_flag = 1;
-    PT_WAIT_UNTIL(thread, thread1_flag != 0);
+    THREAD_WAIT_UNTIL(t, thread1_flag != 0);
     printf("Thread 2 is running\n");
     thread1_flag = 0;
-
   }
-  THREAD_END(thread);
+  THREAD_END(t);
 }
 
 int main(void) 
@@ -40,8 +38,8 @@ int main(void)
   THREAD_INIT(&thread2);
   
   while(1) {
-    thread1(&thread1);
-    thread2(&thread2);
+    thread1_function(&thread1);
+    thread2_function(&thread2);
   }
 }
 
--- unraveled/Anusha/example/thread.h
+++ unraveled/Anusha/example/thread.h
@@ -1,38 +1,38 @@
 #ifndef _THREAD_H_
 #define __THREAD_H_
 
-// Switch statement based implementation of local continuation
-// based protothreading architecture.
-/** \hideinitializer */
+// Based heavily on protothreading example.
+
 typedef void * pass_t;
-
 #define PASS_INIT(s) s = NULL
-
-#define PASS_RESUME(s)        \
-  do {            \
+#define PASS_RESUME(s) \
+  do {\
     if(s != NULL) {       \
       goto *s;          \
     }           \
   } while(0)
-
 #define PASS_CONCAT2(s1, s2) s1##s2
 #define PASS_CONCAT(s1, s2) LC_CONCAT2(s1, s2)
-
 #define PASS_SET(s)       \
   do {            \
     PASS_CONCAT(PASS_LABEL, __LINE__):            \
     (s) = &&PASS_CONCAT(PASS_LABEL, __LINE__);  \
   } while(0)
-
 #define PASS_END(s)
 
-// Taken from protothreading example.
 // A thread just contains one of these objects.
 typedef struct thread {
   pass_t pass;
 } thread;
 
+// Scheduling is hard.
 #define THREAD_SCHEDULE(f) ((f) < THREAD_EXITED)
-void THREAD_INIT(thread* thread);
+
+void THREAD_INIT(struct thread* thread);
+void THREAD_BEGIN(struct thread* thread);
+int THREAD_WAIT_UNTIL(struct thread* thread, int condition);
+int THREAD_YIELD(struct thread* thread);
+int THREAD_END(struct thread* thread);
+int THREAD_EXIT(struct thread* thread); 
 
 #endif

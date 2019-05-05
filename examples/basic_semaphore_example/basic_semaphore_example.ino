0b489e0b57e6970a78343d40b321bc624038c3df
Anusha Datar <anushadatar@gmail.com>
2019-05-04 20:19:40 -0400

    do you ever just misspell initialize 40000 times

--- examples/basic_semaphore_example/basic_semaphore_example.ino
+++ examples/basic_semaphore_example/basic_semaphore_example.ino
@@ -12,68 +12,39 @@ static int buffer[BUFFER_SIZE];
 static int buffer_pointer;
 static struct semaphore full, empty;
 
-static void add_buffer(int value)
-{
-  Serial.print("Value ");
-  Serial.print(value);
-  Serial.print (" added at ");
-  Serial.print(buffer_pointer);
-  Serial.print("\n");
-  buffer[buffer_pointer] = value;
-  buffer_pointer = (buffer_pointer + 1) % BUFFER_SIZE;
-}
-static int get_buffer(void)
-{
-  int value;
-  value = buffer[buffer_pointer];
-  Serial.print("Value ");
-  Serial.print(value);
-  Serial.print (" removed at ");
-  Serial.print(buffer_pointer);
-  Serial.print("\n");
-  buffer_pointer = (buffer_pointer + 1) % BUFFER_SIZE;
-  return value;
-}
-
-static int create_value(void)
-{
-  static int value = 0;
-  Serial.print("Value ");
-  Serial.print(value);
-  Serial.print (" created.");
-  Serial.print("\n");  
-  return value++;
-}
-
-static void remove_value(int value)
-{
-  Serial.print("Value ");
-  Serial.print(value);
-  Serial.print (" removed.");
-  Serial.print("\n");
-}
- 
-static void consumer(struct thread* thread) {
+static int consumer(struct thread* thread) {
   static int total_consumed;
     
   THREAD_START(thread);
  
   for(total_consumed = 0; total_consumed < NUM_ITEMS; ++total_consumed) {
     SEMAPHORE_WAIT(thread, &empty);
-    remove_value(get_buffer());    
+    int value;
+    value = buffer[buffer_pointer];
+    Serial.print("Value ");
+    Serial.print(value);
+    Serial.print (" removed at ");
+    Serial.print(buffer_pointer);
+    Serial.print("\n");
+    buffer_pointer = (buffer_pointer + 1) % BUFFER_SIZE;
     SEMAPHORE_SIGNAL(thread, &full);
   }
   THREAD_CLEAR(thread);
 }
 
-static void producer(struct thread *thread) {
+static int producer(struct thread *thread) {
   static int total_produced;
   THREAD_START(thread);
   for(total_produced = 0; total_produced < NUM_ITEMS; ++total_produced) {  
     SEMAPHORE_WAIT(thread, &empty); 
-    int value = create_value();  
-    add_buffer(value);
- 
+    int value = 1;  
+  Serial.print("Value ");
+  Serial.print(value);
+  Serial.print (" added at ");
+  Serial.print(buffer_pointer);
+  Serial.print("\n");
+  buffer[buffer_pointer] = value;
+  buffer_pointer = (buffer_pointer + 1) % BUFFER_SIZE; 
     
     SEMAPHORE_SIGNAL(thread, &full);
   }
@@ -83,10 +54,10 @@ static void producer(struct thread *thread) {
  
 static thread main_thread(struct thread *thread)
 {
-  static struct pt pt_producer, pt_consumer;
+  static struct thread producer_thread, consumer_thread;
   THREAD_START(thread);
   SEMAPHORE_INIT(&empty, 0);
-  SEMAPHORE_INIT(&full, BUFSIZE);
+  SEMAPHORE_INIT(&full, BUFFER_SIZE);
   THREAD_INITIALIZE(&producer_thread);
   THREAD_INITIALIZE(&consumer_thread);
   THREAD_WAIT_THREAD(thread, producer(&producer_thread) &
